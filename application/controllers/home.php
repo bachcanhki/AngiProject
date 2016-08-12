9799c5729fb17eea69b23a819d48d8b60b4ffd9a
hieuvh <vuhoanghieu17894@gmail.com>
2016-08-12 02:36:08 -0500

    Sửa signup home

--- application/controllers/home.php
+++ application/controllers/home.php
@@ -383,7 +383,8 @@ class Home extends CI_Controller {
 //                $ok = 0;
 //                $error .= 'Chưa nhập địa chỉ<br />';
             }
-            
+            if($memBirthDay == '') $memBirthDay=date("d/m/Y");
+
             if ($ok == 0){
                 $model['error'] = $error; 
                 $model['district'] = $this->addressModel->FindDistrictByProvinceId($provinceID);
