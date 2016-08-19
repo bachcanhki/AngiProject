7349fc57dd1a44d5cb2435421b2342695bf1576c
hieuvh <vuhoanghieu17894@gmail.com>
2016-08-19 14:39:26 +0700

    update restaurant view

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://www.angi.com.local/';
+$config['base_url'] = 'http://localhost:8080/AnGiProjectvsCanh/';
 
 /*
 |--------------------------------------------------------------------------
